04a695d9acc3fb0592b19b9a87f2855314335e84
adityabisoi <adityabisoi1999@gmail.com>
2020-03-02 15:25:17 +0530

    build

---
+++ build/precache-manifest.3e6e5f3673059a10c19ddc6807e18a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e834eb2e9bc6a3781533d588e3f4739f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8ecb95d6ffcafb5363",
+    "url": "/static/css/main.8dcf305d.chunk.css"
+  },
+  {
+    "revision": "acb5a924e715923030fc",
+    "url": "/static/js/2.5dbfa1d3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5dbfa1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8ecb95d6ffcafb5363",
+    "url": "/static/js/main.df8b9213.chunk.js"
+  },
+  {
+    "revision": "0375b034fe2cffb9447f",
+    "url": "/static/js/runtime-main.440187ab.js"
+  }
+]);
