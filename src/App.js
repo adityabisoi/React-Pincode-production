4019cccf7e612bbcdff826f42b80e0101a699981
adityabisoi <adityabisoi1999@gmail.com>
2020-03-05 18:22:01 +0530

    header improved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Pincode from "./Pincode";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Pincode />
     </div>
   );
