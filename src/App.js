ab1f162a7a6be0a0cce7ae0aa6ce7bd7cbb74786
adityabisoi <adityabisoi1999@gmail.com>
2020-03-08 22:45:57 +0530

    ui improved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./App.css";
-import Pincode from "./Pincode";
 import Header from "./Header";
+import FormPage from "./FormPage";
+import StickyFooter from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Pincode />
+      <FormPage/>
+      <StickyFooter/>
     </div>
   );
 }
--- src/FormPage.js
+++ src/FormPage.js
@@ -1,74 +1,108 @@
 import React, { Component } from "react";
 import axios from "axios";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import './App.css'
+
 class Pincode extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       pincode: "",
       city: "",
-      state: ""
+      state: "",
+      error: ""
     };
   }
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
     if (e.target.value.length === 6) {
+      this.setState({
+        error: ""
+      });
       axios
         .get(`https://api.postalpincode.in/pincode/${e.target.value}`)
         .then(res =>
           this.setState({
             state: res.data[0].PostOffice[0].State,
-            city: res.data[0].PostOffice[0].Region
+            city: res.data[0].PostOffice[0].District
           })
         )
         .then(() => {
           document.getElementById("pincode").classList.remove("error");
         })
         .catch(err => {
-          document.getElementById("pincode").className = "error";
+          // document.getElementById("pincode").className = "error";
+          this.setState({
+            error: "Invalid PIN Code"
+          });
         });
     }
-    if (e.target.value.length < 6) {
+    if (e.target.value.length !== 6) {
       this.setState({
         city: "",
-        state: ""
+        state: "",
+        error: "ZIP code must be of 6 digits"
       });
     }
   }
   render() {
     return (
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
       <div style={this.props.Container}>
+      {this.state.error ? (
+          <span className="error-display">{this.state.error}</span>
+        ) : null}
         <div style={this.props.pincodeContainer}>
-          <input
-            maxLength={6}
-            minLength={6}
+        <form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            label="Pin Code"
+            autoFocus
+            inputProps={{ maxLength: 6 }}
             onChange={e => this.onChange(e)}
             name="pincode"
-            placeholder="Pin Code"
             value={this.state.pincode}
             id="pincode"
             style={this.props.pincodeInput}
           />
-        </div>
-        <div style={this.props.cityContainer}>
-          <input
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="city"
+            label="City"
+            id="city"
             type="String"
             disabled={true}
             placeholder="City"
             value={this.state.city}
             style={this.props.cityInput}
           />
-        </div>
-        <div style={this.props.stateContainer}>
-          <input
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="State"
+            label="State"
+            id="State"
             type="String"
             placeholder="State"
             disabled={true}
             value={this.state.state}
             style={this.props.stateInput}
           />
-        </div>
+        </form>
       </div>
+      </div>
+      <Box mt={8}>
+      </Box>
+    </Container>
     );
   }
 }
